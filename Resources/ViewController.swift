6644bf55cc0c8203db8ec04bd375422f4bffcb67
Unknown <ingermanm@hotmail.com>
2018-06-02 15:52:16 -0600

    Version 2

--- Resources/ViewController.swift
+++ Resources/ViewController.swift
@@ -13,13 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderControl: UISlider!
     @IBOutlet weak var labelControl: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBAction func sliderPogress(_ sender: UISlider) {
     
         let currentValue = Int(sender.value)
         
         labelControl.text = "\(currentValue)"
-        
+        }
+
+
+
+
+    @IBAction func rotateImave(_ sender: Any) {
+    self.imageView.transform = CGAffineTransform(rotationAngle: (180.0 * .pi) / 180.0)
+
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
