8ed7ca0bc41d4698391a484e51b53ea879a3dd4c
Inger Montenegro <ingermanm@hotmail.com>
2018-06-02 15:24:56 -0600

    Initial Commit

---
+++ Resources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Resources
+//
+//  Created by Inger Montenegro on 6/2/18.
+//  Copyright © 2018 Inger Montenegro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
